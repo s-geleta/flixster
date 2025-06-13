32ec15c90f07c70234e1311d91053f22466d8cb1
Senaf Geleta <senafg19@gmail.com>
2025-06-13 13:52:43 -0700

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import axios from 'axios'
 import MovieList from './Components/MovieList/MovieList'
 import MovieCard from './Components/MovieCard/MovieCard'
+import Header from './Components/Header/Header'
 
 const App = () => {
   return (
     <>
     <div className="App">
+      <Header />
       <MovieList />
     </div>
     </>
