5818d1a1defa3bade732c61706001d9e426f12a1
Senaf Geleta <senafg19@gmail.com>
2025-06-13 13:37:43 -0700

    added load more button functionality

--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -7,18 +7,19 @@ import "./MovieList.css";
 
 const MovieList = () => {
     const [movies, setMovies] = useState([]);
-
+    const [page , setPage] = useState(1);
+    
     useEffect(() => {
         const fetchList = async () => {
             try{
                 const { data } = await axios.get("https://api.themoviedb.org/3/movie/now_playing", {
-                params: {language: 'en-US', page: '1'},
+                params: {language: 'en-US', page:page },
                 headers: {
                 Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTgyODQxYThiMTBhZGNiNWVlZTE2MzQ0NTA3OWEwMSIsIm5iZiI6MTc0OTgzODMzNC43MTYsInN1YiI6IjY4NGM2OWZlNWQwZmNmNzczMDVjNzQ3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.G7-mS54v47mBxzyXaLV2rBQfax1lgZpD1odYnBoIjws`,
                 accept: "application/json",
                 },
             });
-                setMovies(data.results);
+                setMovies((prevMovies)=>[...prevMovies, ...data.results]);
 
             }
             catch(err){
@@ -26,8 +27,10 @@ const MovieList = () => {
             }
             
         };
+        
+        
         fetchList();
-    },[]);
+    },[page]);
 
     return (
         <>
@@ -41,6 +44,9 @@ const MovieList = () => {
 
             ))}
         </div>
+        <button onClick={() => setPage(page + 1)}>Next Page</button>
+
+    
         </>
     );
 
